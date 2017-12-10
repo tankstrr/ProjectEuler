b7f9ee056d831d11fa8c71a8a1882e58005a90ec
Ron Aebig <tankstrr@gmail.com>
2017-12-10 15:41:19 -0700

    2 tries

---
+++ 001 - Multiples of 3 or 5/001 - Multiples of 3 or 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001___Multiples_of_3_or_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSum = 0;
+            int toCheck = 1000;
+            for (int i = 1; i < toCheck; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    totalSum = totalSum + i;
+                }else if (i % 5 == 0)
+                {
+                    totalSum = totalSum + i;
+                }
+            }
+            Console.WriteLine("Total Sum: {0}", totalSum);
+            Console.ReadKey();
+        }
+    }
+}
