6f2a0da5e58405864898b027e5d0955dd00881d0
Ron Aebig <tankstrr@gmail.com>
2017-12-13 21:05:07 -0700

    007 & 010

---
+++ 007 - 10001st Prime/007 - 10001st Prime/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007___10001st_Prime
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int zl = 3;
+            int var = 2;
+            while (var <= 10001)
+            {
+
+                for (int i = 2; i < zl; i++)
+                {
+                    if (zl % i != 0)
+                    {
+
+                        if (i == zl - 1)
+                        {
+                            Console.WriteLine(var + " " + zl);
+                            var++;
+                        }
+                        continue;
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+                zl++;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 010 - Summation of Primes/010 - Summation of Primes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010___Summation_of_Primes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            long i, j, sum = 0;
+            bool flag = true;
+            for (i = 2; i < 2000000; i++)
+            {
+                flag = true;
+                for (j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                    }
+                }
+                if (flag == true)
+                {
+                    sum = sum + i;
+                }
+
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
