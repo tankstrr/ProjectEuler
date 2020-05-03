9dd0af45dd04e17dce779d28fe41f716354b02b2
Ron Aebig <tankstrr@gmail.com>
2020-05-02 21:14:21 -0600

    048 - Not working, biginteger crashed on 143^143

---
+++ 048 - Self powers/048 - Self powers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _048___Self_powers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger totalSum = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                BigInteger newNum;
+                double power = Math.Pow(i, i);
+                newNum = (BigInteger) power;
+                totalSum = totalSum + newNum;
+                Console.WriteLine("{0} - {1}", i, newNum);
+                Console.WriteLine("Total Sum: {0}", totalSum);
+            }
+        }
+    }
+}
