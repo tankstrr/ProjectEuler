2911087a2004c18a00bb7392577bf7097ecebe46
Ron Aebig <tankstrr@gmail.com>
2020-05-02 20:13:13 -0600

    Had to use double instead of int

---
+++ 014 - Longest Collatz Sequence/014 - Longest Collatz Sequence/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014___Longest_Collatz_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Longest Collatz Sequence");
+            Console.WriteLine("======= ======= ========");
+            double checkNum = 1000000;
+            int maxSteps = 0;
+            int maxStepsNum = 0;
+            for (int i = 1; i <= checkNum; i++)
+            {
+                double j = i;
+                int steps = 1;
+//                Console.Write("{0}", j);
+                while (j > 1)
+                {
+                    if (j % 2 == 0)
+                    {
+                        j = j / 2;
+                    }
+                    else
+                    {
+                        j = (j * 3) + 1;
+                    }
+//                    Console.Write(" -> {0}", j);
+                    steps++;
+                }
+//                Console.WriteLine(" ({0} steps)", steps);
+                if (steps > maxSteps)
+                {
+                    maxSteps = steps;
+                    maxStepsNum = i;
+                    Console.WriteLine("{0} steps for {1}", maxSteps, maxStepsNum);
+                }
+                if (i % 10000 == 0)
+                {
+//                    Console.WriteLine("Checked {0}", i);
+                }
+            }
+            Console.WriteLine("===============");
+            Console.WriteLine("Largest Chain: {0}", maxSteps);
+            Console.WriteLine("          Num: {0}", maxStepsNum);
+        }
+    }
+}
